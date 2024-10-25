523ed7bdbaf2ae92c00df75dfc0baf71dc5c3d0b
Kira <kafountain5@gmail.com>
2024-10-25 19:37:16 -0400

    test

--- Assets/Scripts/Npc.cs
+++ Assets/Scripts/Npc.cs
@@ -7,16 +7,5 @@ public class Npc : MonoBehaviour
 {
     private DescantDialogueTrigger dialogueTrigger;
 
-    [SerializeField] private string name;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private string name = "Joe";
 }
